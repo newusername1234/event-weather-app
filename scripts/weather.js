84205bf1fd687ec0c924d41a2732737444574974
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2019-12-16 14:43:34 -0500

    Adds css styling for result containers

--- scripts/weather.js
+++ scripts/weather.js
@@ -30,7 +30,7 @@ function findWeatherAndTempforDateGiven (dateGiven, newCard) {
             if (item.dt_txt == dateGiven) {
                 appendTextToCard(item.weather[0].description.toUpperCase(), newCard, "h3");
                 appendTextToCard(String(kToF(item.main.temp)), newCard, "h4");
-                appendImagetoCard("http://openweathermap.org/img/w/" + String(item.weather[0].icon) + ".png", newCard);
+                appendIcontoCard("http://openweathermap.org/img/w/" + String(item.weather[0].icon) + ".png", newCard);
             }
         }
     })  
@@ -45,7 +45,7 @@ function findCurrentWeather(newCard) {
     .then(function (obj) {
         appendTextToCard(getDescription(obj).toUpperCase(), newCard, "h3");
         appendTextToCard(String(getTemperature(obj)), newCard, "h4");
-        appendImagetoCard(getIcon(obj), newCard);
+        appendIcontoCard(getIcon(obj), newCard);
     })
         // .then(showMeTHing)
 }
